06066ba34e261fe2a45ffa80f1563b6f943f08c5
Pedro Elias <106087744+Elias-Pdr@users.noreply.github.com>
2022-09-23 18:35:11 -0300

    up

--- conexao.php
+++ conexao.php
@@ -1,8 +1,9 @@
 <?php
-$servidor = "localhost";
-$usuario = "root";
-$senha = "";
-$dbname = "celke";
+
+$servidor = "ec2-3-93-206-109.compute-1.amazonaws.com";
+$usuario = "ygqleifgediaff";
+$senha = "e0c86b895a39c1f2b64f21428c389e40881a55116f03997497c2467b88b86893";
+$dbname = "d7ehhfm8gtr48s";
 
 //Criar a conexao
 $conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
